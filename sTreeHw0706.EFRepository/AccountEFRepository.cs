a4914ddf456c5683528d4820e92e857145814dd2
Lin Chenyu <04156204@gm.scu.edu.tw>
2019-07-15 11:59:49 +0800

    加入Services 加入GetAll方法

--- sTreeHw0706.EFRepository/AccountEFRepository.cs
+++ sTreeHw0706.EFRepository/AccountEFRepository.cs
@@ -8,5 +8,20 @@ namespace sTreeHw0706.EFRepository
 {
     public class AccountEFRepository
     {
+        public List<AccountBook> GetAll()
+        {
+            using (SkillTreeHomeworkEntities context = new SkillTreeHomeworkEntities())
+            {
+                return context.AccountBook.ToList();
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
--- sTreeHw0706/Controllers/AccountController.cs
+++ sTreeHw0706/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using sTreeHw0706.EFRepository;
 
 namespace sTreeHw0706.Controllers
 {
